e56e6fe0cc42488643993e694efa956ba8266dd1
Pato Zavala Fuenzalida <49573463+pazavala@users.noreply.github.com>
2019-06-25 11:58:09 -0400

    Update element shape functions 2D.py
    Some corrections

--- element shape functions 2D.py
+++ element shape functions 2D.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-def tri_gauss_quad(k):
+
+def gauss_quadrature_triangle(k):
     """
-    this rotuine gives the quadrature points and weights of quadrature to
+    this routine gives the quadrature points and weights of quadrature to
     integrate numerically a polynomial of order "k".
     :param k: polynomial order to approximate
     :return: xi :  coordinates of quad points
@@ -10,15 +11,12 @@ def tri_gauss_quad(k):
     """
 
     if k == 1:
-        # 1st order ---> 1 quad point
         xi = np.array([[1 / 3, 1 / 3]])
         w = np.array([0.5])
     elif k == 2:
-        # 2nd order ---> 3 quad points
         xi = np.array([[2 / 3., 1. / 6.], [1. / 6., 2. / 3.], [1. / 6., 1. / 6.]])
         w = np.array([1. / 6., 1. / 6., 1. / 6.])
     elif k == 3:
-        # 3rd order ---> 4 quad points
         xi = np.array([[1. / 3., 1. / 3.], [3. / 5., 1. / 5.], [1. / 5., 3. / 5.], [1. / 5., 1. / 5.]])
         w = np.array([-27. / 96., 25. / 96., 25. / 96., 25. / 96.])
     return xi, w
@@ -28,10 +26,10 @@ def p1_shape_function(triangle, xi):
     """
     this routine evaluates the shape functions and their spatial gradients
     at the quadrature points in the iso-parametric configuration for P1-elements
-    :param x: nodes of the of the elements in non-iso parametric configuration
-    :param n: order of quadrature
-    :return: Np : values ​​of the function as assessed at the quad point.
-             DN : gradient of Np
+    :param triangle: nodes of the of the elements in non-iso parametric configuration
+    :param xi: point of quadrature
+    :return: N : values ​​of the function as assessed at the quad point.
+             DN : gradient of N
              J  : Jacobian
     """
 
@@ -48,6 +46,15 @@ def p1_shape_function(triangle, xi):
 
 
 def p2_shape_function(triangle, xi):
+    """
+    this routine evaluates the shape functions and their spatial gradients
+    at the quadrature points in the iso-parametric configuration for P2-elements
+    :param triangle: nodes of the of the elements in non-iso parametric configuration
+    :param xi: point of quadrature
+    :return: N : values of the function as assessed at the quad point.
+             nabla_N : gradient of N
+             J  : Jacobian
+    """
     xi1, xi2 = xi
     xi3 = 1 - xi1 - xi2
     n1hat = xi1 * (2. * xi1 - 1.)
@@ -64,11 +71,12 @@ def p2_shape_function(triangle, xi):
     return N, nabla_N, J
 
 
+
 """
 Execution
 """
 n = 2
-xi = tri_gauss_quad(n)[0][0]
+xi = gauss_quadrature_triangle(n)[0][0]
 x_p1 = np.array([[0, 0], [0, 3], [5, 0]]).T
 x_p2 = np.array([[0, 0], [0, 1.5], [2.5, 0], [2.5, 1.5], [0, 3], [5, 0]]).T
 
